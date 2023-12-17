899add2b154423e91647efbcfa23e98817c5a542
Erisha Roach <forever.erisha@gmail.com>
2023-12-17 03:22:49 -0500

    can add an task to list. working on removing task

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('taskInput');
+    const toDoForm = document.getElementById('toDoForm');
+    const taskList = document.getElementById('taskList');
+    const container = document.querySelector('#app');
+
+    //Event listeners
+    toDoForm.addEventListener('submit', addTask);
+
+    // function to add a new task
+    function addTask(event){
+        event.preventDefault();
+
+        const taskTxt = taskInput.value;
+
+        //create task element
+
+        const taskElement = document.createElement('li');
+        taskElement.className = 'task';
+        taskElement.innerHTML = `
+        <span>${taskTxt}</span>
+        <button> Delete</button>
+        `;
+
+        // add new elements to DOM using appendChild
+        taskList.appendChild(taskElement);
+
+        // clear input field
+        taskInput.value = '';
+  
+    }
+
+    // remove a task
+    function removeTask(event) {
+        if (event.target.tagName === 'BUTTON'){
+            const taskElement = event.target.closest('.task');
+
+            taskList.removeChild(taskElement);
+        }
+    }
+    
+})
