12592ae40cac189af520af67515b2e52d6a6bc5b
Erisha Roach <forever.erisha@gmail.com>
2023-12-20 02:34:07 -0500

    form works. working on how form shows up. goig to add form validation next

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('taskInput');
     const toDoForm = document.getElementById('toDoForm');
     const taskList = document.getElementById('taskList');
-    const container = document.querySelector('#container');
+    const app = document.querySelector('#app');
 
     //Event listeners
     toDoForm.addEventListener('submit', addTask);
     taskList.addEventListener('click', removeTask);
+
+    
 
     // function to add a new task
     function addTask(event){
@@ -19,8 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const taskElement = document.createElement('li');
         taskElement.className = 'task';
         taskElement.innerHTML = `
-        <span>${taskTxt}</span>
-        <button> Delete</button>
+            <span>${taskTxt}</span>
+            <button> Delete</button>
         `;
 
         // add new elements to DOM using appendChild
@@ -39,5 +41,35 @@ document.addEventListener('DOMContentLoaded', () => {
             taskList.removeChild(taskElement);
         }
     }
-    
-})
+
+    // documentFragment creates list of tasks
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < 10; i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Task # ${i + 1}`;
+        fragment.appendChild(listItem);
+    }
+    taskList.appendChild(fragment);
+
+// user interactions
+app.addEventListener('mouseover' , () => {
+    app.innerHTML = '<h1> Mover Over Event!</h1>';
+});
+
+taskList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'SPAN'){
+        event.target.style.textDecoration = 'line-through';
+    }
+});
+
+app.addEventListener('mouseout', () => {
+    app.setAttribute('style', 'background-color: lightblue;');
+});
+
+// BOM properties/methods
+
+console.log('Window inner width:', window.innerWidth);
+console.log('Document URL:', document.URL);
+
+
+});
